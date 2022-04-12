d00a0a5f00012e4eb88328fd654c4f7707381a9e
Nam Nguyen <itnams12@gmail.com>
2022-04-12 15:07:17 +0700

    build: create project

---
+++ MoneyManager/MoneyManager/MoneyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManagerApp.swift
+//  MoneyManager
+//
+//  Created by ERM on 12/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
